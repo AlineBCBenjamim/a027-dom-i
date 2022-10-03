6d350720461bbb851a7dfbbc0b8cd4ff78419670
Aline Carvalho <alineoak9@gmail.com>
2022-10-03 20:48:43 -0300

    Exercicio

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+//                  0         1       2
 const frutas = ["laranja", "limão", "uva"];
+
+let fruta1 = document.getElementById("fruta-1")
+fruta1.innerHTML += frutas[0]
+
+let fruta2 = document.getElementById("fruta-2")
+fruta2.innerHTML += frutas[1]
+
+let frutas3 = document.getElementById("fruta-3")
+frutas3.innerHTML += frutas[2]
+
+let imprimaFrutaNoConsole = document.getElementById("fruta")
+console.log(imprimaFrutaNoConsole.value);
+
+
+function aparecerNaTela(){
+    let fruta4 = document.getElementById("fruta-4")
+    fruta4.innerHTML = imprimaFrutaNoConsole.value
+}
